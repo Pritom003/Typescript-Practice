dadcdbe676d71fb9da7d396f2b674cc37ad16a2e
Pritom003 <njahanpritom65@gmail.com>
2024-11-05 17:30:53 +0600

    first commit

---
+++ src/basic.ts
@@ -0,0 +1,16 @@
+const examplearray:number[] =[1,2,3,4]//array
+const exampleTuple:[string,number,boolean]=["name",2,true]//tuple
+// object
+type PersonType={
+    name: string;
+    age: number;
+    company: string;
+    Ismarried: boolean;
+} 
+const Person:PersonType={
+    name:"John",
+    age:33,
+    company:'x',
+    Ismarried:true
+}
+// normal function arrow function
---
+++ src/function.ts
@@ -0,0 +1,12 @@
+function add (numb1:number ,numb2:number):number{
+    return numb1+numb2
+}
+const result =add(2,3)
+console.log(result);
+const multiplicaton =(a:number ,  b :number):string =>{
+    const result =a*b
+    return ` the result is ${result}`
+
+}
+const result2= multiplicaton(2433,42343)
+console.log(result2);
