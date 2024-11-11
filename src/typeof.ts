8fd3e580ecd8af8ab67d731888d2f5e0b5d72e55
Pritom003 <njahanpritom65@gmail.com>
2024-11-11 22:52:08 +0600

    added a md fiel

---
+++ src/typeof.ts
@@ -0,0 +1,23 @@
+{
+    // A string literal type is a type that represents an exact, specific string. When a variable is assigned a string literal type, it can only hold that specific string value.
+const Tname ='something'
+type personName=typeof Tname 
+const name :personName='something'// it has to be the same string  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
